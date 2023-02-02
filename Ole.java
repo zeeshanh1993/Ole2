332783cecaa7a1d6d83b779ac12d495281f6ce4c
zeeshanh1993 <zeeshan.h1993@gmail.com>
2023-02-02 14:26:32 +0500

    This is the second commit

--- Ole.java
+++ Ole.java
@@ -1,5 +1,7 @@
 public class Ole {
     public static void main(String[] args) {
         System.out.println("Hello, World!!!");
+        System.out.println("This is out first github class")
+        
     }
 }
