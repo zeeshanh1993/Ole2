9e8adec9550d292bc8afa66126d2f598bd887131
zeeshanh1993 <zeeshan.h1993@gmail.com>
2023-02-04 15:30:05 +0500

    This is a new commit

--- Newtry.java
+++ Newtry.java
@@ -1,5 +1,9 @@
 public class Newtry {
     public static void main(String[] args) {
         System.out.println("Hello there!");
+        int x = 7;
+        int z = 9;
+        System.out.println(x + z);
+
     }
 }
